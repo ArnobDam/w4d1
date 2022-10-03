6af1fc9866ed579595f38c94a9926c3750b23326
Arnob <arnobdam@gmail.com>
2022-10-03 15:46:50 -0400

    started children in tictactoe

--- skeleton/lib/tic_tac_toe_node.rb
+++ skeleton/lib/tic_tac_toe_node.rb
@@ -1,7 +1,13 @@
 require_relative 'tic_tac_toe'
+require "byebug"
 
 class TicTacToeNode
+  attr_reader :board, :next_mover_mark
   def initialize(board, next_mover_mark, prev_move_pos = nil)
+    @board = board
+    # @next_mover_mark == :x ? @next_mover_mark = :o : @next_mover_mark = :x
+    @next_mover_mark = next_mover_mark
+    @prev_move_pos = prev_move_pos
   end
 
   def losing_node?(evaluator)
@@ -13,5 +19,26 @@ class TicTacToeNode
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
+    # @next_mover_mark == :x ? @next_mover_mark = :o : @next_mover_mark = :x
+    
+    # debugger
+    # arr = @board.rows
+    orig_dup = @board.dup
+    (0..2).each do |i|
+      (0..2).each do |j|
+        if @board.[[i,j]].nil?
+          arr = orig_dup
+          new_board = deep_dup(arr)
+          new_board[i][j] = @next_mover_mark
+          @board[[i,j]] = TicTacToeNode.new(new_board, @next_mover_mark, [i,j])
+        end
+      end
+    end
+    # @board.rows.flatten
   end
+
+  # def deep_dup(board)
+  #   # arr = board.rows
+  #   board.map {|ele| ele.is_a?(Array) ? deep_dup(ele) : ele}
+  # end
 end
