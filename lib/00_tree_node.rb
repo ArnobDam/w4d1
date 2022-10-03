a9c61a68ba00c16337633ef65233b3035b83b93b
Gleb Mirzayev <gmirzayev@gmail.com>
2022-10-03 11:37:14 -0400

    initial commit

---
+++ lib/00_tree_node.rb
@@ -0,0 +1,69 @@
+class PolyTreeNode
+    attr_reader(:parent, :children, :value)
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
+
+    def parent=(parent_node)
+        if @parent != nil
+            @parent.children.delete(self)
+        end
+
+        @parent = parent_node
+        @parent.children << self unless @parent == nil
+    end
+
+    def add_child(child_node)
+        child_node.parent=self
+    end
+
+    def remove_child(child_node)
+        raise "not a child" if child_node.parent == nil
+        child_node.parent=nil
+    end
+
+    def dfs(target_value)
+        return self if value == target_value
+
+        children.each do |child|
+            child_node = child.dfs(target_value)
+            return child_node if child_node != nil
+        end
+
+        nil
+    end
+
+    def bfs(target_value)
+        queue_arr = []
+
+        queue_arr.push(self)
+
+        until queue_arr.empty?
+            ele = queue_arr.shift
+            return ele if ele.value == target_value
+            ele.children.each do |child|
+                queue_arr.push(child)
+            end
+        end
+
+        nil
+    end
+
+    # def inspect
+    #     puts
+    #     p value
+    #     children.each {|child| p child.value}
+    # end
+end
+
+# [[6,3],[4,2],[2,1],[0,0]]
+# if ele.value == target.value
+#     move ele into an array
+#     while array[-1].parent != nil
+#         move array[-1].parent into array
+#     end
+#     return array
+# end
+
---
+++ lib/knights_travails.rb
@@ -0,0 +1,59 @@
+require_relative '00_tree_node.rb'
+
+class KnightPathFinder
+    def self.valid_moves(position)
+        move_array = []
+
+        all_position_changes = [[1,2],[-1,2],[2,1],[2,-1],[1,-2],[-1,-2],[-2,1],[-2,-1]]
+        all_position_changes.each do |position_change|
+            new_position = [position_change[0] + position[0], position_change[1] + position[1]]
+            if new_position[0] < 7 && new_position[0] >= 0 && new_position[1] < 7 && new_position[1] >= 0
+                move_array << new_position
+            end
+        end
+        move_array
+    end
+
+    def initialize(position)
+        @root_node = PolyTreeNode.new(position)
+        @considered_positions = [position]
+    end
+
+    def new_move_positions(pos)
+        valid_positions = KnightPathFinder.valid_moves(pos)
+        new_positions = []
+
+        valid_positions.each do |position|
+            if !@considered_positions.include?(position)
+                @considered_positions << position
+                new_positions << position
+            end
+        end
+        new_positions
+    end
+
+    def build_move_tree
+        move_queue = []
+        move_queue << @root_node
+
+        until move_queue.empty?
+            shifted_node = move_queue.shift
+            move_positions = new_move_positions(shifted_node.value)
+            move_positions.each do |pos|
+                node = PolyTreeNode.new(pos)
+                node.parent = shifted_node
+                move_queue << node
+            end
+        end
+        @root_node
+    end
+
+    
+end
+
+k = KnightPathFinder.new([0,0])
+
+tree = k.build_move_tree
+
+p tree
+
